c6830c883beedd431ca933eabb1b8a67f4b86e6e
野村　俊介 <nomura@shunn.local>
2021-05-06 23:22:23 +0900

    update chart

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -21,30 +21,54 @@ export default {
     updateChartData: function () {
       this.datacollection = {
         labels: ['5/1', '5/2', '5/3', '5/4', '5/5', '5/6', '5/7'],
-        datasets: [{
-          label: 'Line Sample 1',
-          backgroundColor: '#f87979',
-          data: [14, 19, 16, 17, 12, 15, 10],
-          lineTension: 0,
-          fill: false
-        }, {
-          label: 'Line Sample 2',
-          backgroundColor: '#fcba03',
-          data: [4, 6, 5, 9, 7, 3, 6],
-          lineTension: 0,
-          fill: false
-        }]
+        datasets: [
+          {
+            label: 'Line Sample 1',
+            borderColor: 'rgba(254,97,132,0.8)',
+            backgroundColor: '#f87979',
+            data: [14, 19, 16, 17, 12, 15, 10],
+            lineTension: 0.3,
+            fill: false
+          }, {
+            label: 'Line Sample 2',
+            borderColor: 'rgba(54,164,235,0.8)',
+            backgroundColor: '#fcba03',
+            data: [4, 6, 5, 9, 7, 3, 6],
+            lineTension: 0.3,
+            fill: false
+          }
+        ]
       };
       this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scaleBeginAtZero: true,
+        title: {
+          display: true,
+          color: 'rgba(54,164,235,0.8)',
+          text: 'Custom Chart Title',
+          fontSize: 24
+        },
+        legend: {
+          display: true,
+          position: 'right',
+          labels: { fontSize: 18 }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              max: 20,
+              min: 0,
+              stepSize: 2,
+              beginAtZero: true
+            }
+          }]
+        },
         plugins: {
           zoom: {
-            pan: {
-              enabled: true,
-              mode: "x"
-            },
             zoom: {
               enabled: true,
-              mode: "x"
+              mode: 'xy'
             }
           }
         }
